d56aff654c66f545bca32ddbd356749af4602601
Sven Wolfermann <sven@maddesigns.de>
2021-08-03 12:02:15 +0200

    chore: update packages

--- postcss.config.js
+++ postcss.config.js
@@ -3,13 +3,6 @@ module.exports = {
 	map: false,
 	plugins: {
 		'autoprefixer': { grid: true },
-		'postcss-clean': {
-			level: {
-				2: {
-					mergeAdjacentRules: false
-				}
-			},
-			//format: 'beautify'
-		}
+		'cssnano': {}
 	}
 }
